b0fb530ce7b0f4fe50068cbdaef653ffcad1ee21
Sean Michael <sean@calibreworks.co.id>
2015-06-27 16:19:42 +0700

    Init

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+define( 'CHILD_THEME_NAME', 'cl0ckw0rks' );
+define( 'CHILD_THEME_URL', 'http://www.cl0ckw0rks.com' );
+define( 'CHILD_THEME_VERSION', '1.0.0' );
+define( 'CHILD_THEME_DB_VERSION', '1' );
+define( 'CHILD_APP_DIR', 'app' );
+
+remove_theme_support( "calibrefx-template-styles" );
+
+
+
